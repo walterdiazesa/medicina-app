d7d3bed0064f5876cbbbce97a1c024ef8b38e863
walterdiazesa <walterwalon@gmail.com>
2022-05-18 17:51:08 -0600

    + Change ws transport to websocket

--- socketio/index.ts
+++ socketio/index.ts
@@ -2,10 +2,12 @@ import { io } from "socket.io-client";
 import { baseURL } from "../axios";
 import { EventType } from "../types/Socketio";
 
-export const socket = io(baseURL.replace("http", "ws").replace("https", "ws"));
+export const socket = io(baseURL.replace("http", "ws").replace("https", "ws"), {
+  transports: ["websocket"],
+});
 export const listen = (
   event: EventType,
   listener: (...args: any[]) => void
 ) => {
-  socket.on(event, listener);
+  socket.off(event).on(event, listener);
 };
