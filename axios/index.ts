a04e49ba0f16580d9f17a48e506a985065f2de48
walterdiazesa <walterwalon@gmail.com>
2022-06-17 23:28:17 -0600

    * Test img cache | Change env.API_HOST > env.NEXT_PUBLIC_API_HOST

--- axios/index.ts
+++ axios/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8080"
-    : process.env.API_HOST!;
+    : process.env.NEXT_PUBLIC_API_HOST!;
 
 export const api = axios.create({
   baseURL,
